4c79573d0b9e9e636ca5db15698eabca934777b5
diegomarprogramacion <diegomarprogramacion@gmail.com>
2023-10-23 14:05:54 +0200

    update quit sitemap import

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,6 @@ import Contact from './pages/Contact/Contact'
 import Blog from './pages/Blog/Blog'
 import About from './pages/About/About'
 import Login from './pages/Login/Login'
-import SiteMap from './pages/SiteMap/sitemap.xml'
 import PageNotFound from './pages/PageNotFound/PageNotFound'
 
 function App() {
