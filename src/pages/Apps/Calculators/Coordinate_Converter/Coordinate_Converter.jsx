9cfd3413e7a8167a26ba9a9df4e585a7e18b2cb0
diegomarprogramacion <diegomarprogramacion@gmail.com>
2023-11-06 17:47:35 +0100

    gaub-kruger

--- src/pages/Apps/Calculators/Coordinate_Converter/Coordinate_Converter.jsx
+++ src/pages/Apps/Calculators/Coordinate_Converter/Coordinate_Converter.jsx
@@ -3,14 +3,22 @@ import './Coordinate_Converter.css';
 import Navbar from '../../../../components/Navbar/Navbar';
 
 function Apps() {
-  const [latitude, setLatitude] = useState(47.564028);
-  const [longitude, setLongitude] = useState(10.762556);
-  const [latitudeDegrees, setLatitudeDegrees] = useState(47);
-  const [longitudeDegrees, setLongitudeDegrees] = useState(10);
-  const [latitudeMinutes, setLatitudeMinutes] = useState(33);
-  const [longitudeMinutes, setLongitudeMinutes] = useState(45);
-  const [latitudeSeconds, setLatitudeSeconds] = useState(50.5008);
-  const [longitudeSeconds, setLongitudeSeconds] = useState(45.2016);
+  // Variables para coordenadas decimales
+  const [latitude, setLatitude] = useState(52.517265);
+  const [longitude, setLongitude] = useState(13.389244);
+
+  // Variables para coordenadas en grados, minutos y segundos
+  const [latitudeDegrees, setLatitudeDegrees] = useState(52);
+  const [longitudeDegrees, setLongitudeDegrees] = useState(13);
+  const [latitudeMinutes, setLatitudeMinutes] = useState(31);
+  const [longitudeMinutes, setLongitudeMinutes] = useState(23);
+  const [latitudeSeconds, setLatitudeSeconds] = useState(2.154);
+  const [longitudeSeconds, setLongitudeSeconds] = useState(21.2784);
+
+  // Variables para coordenadas Gauß-Krüger
+  const [zone, setZone] = useState(4); // Ejemplo, debes establecer la zona correcta
+  const [r, setR] = useState(4594410.412);
+  const [h, setH] = useState(5821363.617);
 
   const updateDecimalDegrees = (lat, lon) => {
     setLatitude(lat);
@@ -36,7 +44,6 @@ function Apps() {
 
   const handleLatitudeChange = (e) => {
     const value = e.target.value;
-    //console.log(value);
     if (!isNaN(value)) {
       updateDecimalDegrees(value, longitude);
       updateDMSFromDecimal(value, longitude);
@@ -88,6 +95,48 @@ function Apps() {
       setLongitudeSeconds(newLonSeconds);
     }
   };
+  const handleZoneChange = (e) => {
+    const value = parseInt(e.target.value, 10);
+    if (!isNaN(value)) {
+      setZone(value);
+      updateDecimalFromGaussKreuger(value, r, h);
+    }
+  };
+  
+  const handleRChange = (e) => {
+    const value = parseFloat(e.target.value);
+    if (!isNaN(value)) {
+      setR(value);
+      updateDecimalFromGaussKreuger(zone, value, h);
+    }
+  };
+  
+  const handleHChange = (e) => {
+    const value = parseFloat(e.target.value);
+    if (!isNaN(value)) {
+      setH(value);
+      updateDecimalFromGaussKreuger(zone, r, value);
+    }
+  };
+  
+  const updateDecimalFromGaussKreuger = (zone, r, h) => {
+    const rho = 57.29577951; // 180/PI
+    const e2 = 0.006719219;
+    const b1 = r / 10000855.7646;
+    const b2 = b1 * b1;
+    const bf = (325632.08677 * b1 * (
+      ((((0.00000562025 * b2 - 0.0000436398) * b2 + 0.00022976983) * b2 - 0.00113566119) * b2 + 0.00424914906) * b2 - 0.00831729565) * b2 + 1) / 3600 / rho;
+    const fa = (zone * 1000000 - 500000) / (6398786.849 / Math.sqrt(1 + Math.pow(Math.cos(bf), 2) * e2));
+    const LAT_Formula = (bf - (Math.pow(fa, 2) * (Math.sin(bf) / Math.cos(bf)) * (1 + Math.pow(Math.cos(bf), 2) * e2) / 2) + (Math.pow(fa, 4) * (Math.sin(bf) / Math.cos(bf)) * (5 + 3 * Math.pow(Math.sin(bf) / Math.cos(bf), 2) + 6 * Math.pow(Math.cos(bf), 2) * e2 - 6 * Math.pow(Math.cos(bf), 2) * e2 * Math.pow(Math.sin(bf) / Math.cos(bf), 2)) / 24)) * rho;
+    const LON_Formula = (fa - (Math.pow(fa, 3) * (1 + 2 * Math.pow(Math.sin(bf) / Math.cos(bf), 2) + Math.pow(Math.cos(bf), 2) * e2) / 6 + (Math.pow(fa, 5) * (1 + 28 * Math.pow(Math.sin(bf) / Math.cos(bf), 2) + 24 * Math.pow(Math.sin(bf) / Math.cos(bf), 4)) / 120)) * rho / Math.cos(bf));
+  
+    // Actualiza las coordenadas decimales
+    setLatitude(LAT_Formula);
+    setLongitude(LON_Formula);
+    updateDMSFromDecimal(LAT_Formula, LON_Formula);
+  };
+  
+  
 
   return (
     <>
@@ -135,7 +184,7 @@ function Apps() {
                 step="any"
                 name="latDegrees"
                 autoComplete="off"
-                className="input-calculator degrees"
+                className="input-calculator-coordinate degrees"
                 value={latitudeDegrees}
                 onChange={(e) => handleDMSChange('latDegrees', e)}
               />
@@ -148,7 +197,7 @@ function Apps() {
                 step="any"
                 name="latMinutes"
                 autoComplete="off"
-                className="input-calculator minutes"
+                className="input-calculator-coordinate minutes"
                 value={latitudeMinutes}
                 onChange={(e) => handleDMSChange('latMinutes', e)}
               />
@@ -161,7 +210,7 @@ function Apps() {
                 step="any"
                 name="latSeconds"
                 autoComplete="off"
-                className="input-calculator seconds"
+                className="input-calculator-coordinate seconds"
                 value={latitudeSeconds}
                 onChange={(e) => handleDMSChange('latSeconds', e)}
               />
@@ -176,7 +225,7 @@ function Apps() {
                 step="any"
                 name="lonDegrees"
                 autoComplete="off"
-                className="input-calculator degrees"
+                className="input-calculator-coordinate degrees"
                 value={longitudeDegrees}
                 onChange={(e) => handleDMSChange('lonDegrees', e)}
               />
@@ -189,7 +238,7 @@ function Apps() {
                 step="any"
                 name="lonMinutes"
                 autoComplete="off"
-                className="input-calculator minutes"
+                className="input-calculator-coordinate minutes"
                 value={longitudeMinutes}
                 onChange={(e) => handleDMSChange('lonMinutes', e)}
               />
@@ -202,11 +251,53 @@ function Apps() {
                 step="any"
                 name="lonSeconds"
                 autoComplete="off"
-                className="input-calculator seconds"
+                className="input-calculator-coordinate seconds"
                 value={longitudeSeconds}
                 onChange={(e) => handleDMSChange('lonSeconds', e)}
               />
               <label className="user-label-calculator">Lon "</label>
+            </div>
+          </div>
+          <span>Gauß-Krüger</span>
+          <div>
+            <div className="input-group-calculator input-group-calculator-gaus">
+              <input
+                required
+                type="number"
+                step="any"
+                name="zone"
+                autoComplete="off"
+                className="input-calculator-coordinate zone"
+                value={zone}
+                onChange={(e) => handleZoneChange(e)}
+              />
+              <label className="user-label-calculator user-label-zone">Zone</label>
+            </div>
+            <div className="input-group-calculator input-group-calculator-gaus">
+              <input
+                required
+                type="number"
+                step="any"
+                name="r"
+                autoComplete="off"
+                className="input-calculator-coordinate seconds"
+                value={r}
+                onChange={(e) => handleRChange(e)}
+              />
+              <label className="user-label-calculator">Rechtswert (N)</label>
+            </div>
+            <div className="input-group-calculator input-group-calculator-gaus">
+              <input
+                required
+                type="number"
+                step="any"
+                name="h"
+                autoComplete="off"
+                className="input-calculator-coordinate seconds"
+                value={h}
+                onChange={(e) => handleHChange(e)}
+              />
+              <label className="user-label-calculator">Hochwert (E)</label>
             </div>
           </div>
         </form>
