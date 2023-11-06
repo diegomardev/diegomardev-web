53c652d9d214951309ff7fae03a38bb34711f530
diegomarprogramacion <diegomarprogramacion@gmail.com>
2023-11-06 18:11:09 +0100

    map maps

--- src/pages/Apps/Calculators/Coordinate_Converter/Coordinate_Converter.jsx
+++ src/pages/Apps/Calculators/Coordinate_Converter/Coordinate_Converter.jsx
@@ -301,6 +301,16 @@ function Apps() {
             </div>
           </div>
         </form>
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2901.894633584818!2d-8.1979672322785!3d42.33739387279807!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd2e7ca83cbedb05%3A0xbe81693f9040032d!2zR2Fsw6FuLCAzNiwgMTUwMDkgQSBDb3J1w7FhLCBMYSBDb3J1w7Fh!5e0!3m2!1ses!2ses!4v1699289414569!5m2!1ses!2ses"
+          width="360"
+          height="360"
+          className='map'
+          allowFullScreen
+          loading="lazy"
+          referrerPolicy="no-referrer-when-downgrade"
+          title="Google Maps"
+        />
       </div>
     </>
   );
