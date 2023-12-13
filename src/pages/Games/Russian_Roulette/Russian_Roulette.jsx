e8b64f16a4d9ae33ed0efb202c28769788707677
diegomarprogramacion <diegomarprogramacion@gmail.com>
2023-12-13 16:56:49 +0100

    add sound

--- src/pages/Games/Russian_Roulette/Russian_Roulette.jsx
+++ src/pages/Games/Russian_Roulette/Russian_Roulette.jsx
@@ -4,13 +4,20 @@ import "./Russian_Roulette.css";
 import Navbar from "../../../components/Navbar/Navbar";
 import confetti from "canvas-confetti";
 import roulette from "/russian_roulette.svg";
-import zIndex from "@mui/material/styles/zIndex";
+import roulette_song from "/roulette.mp3";
+import reload_shot_song from "/reload_shot.mp3";
+
 
 const Russian_Roulette = () => {
   const [isSpinning, setSpinning] = useState(false);
   const [randomSpins, setRandomSpins] = useState(0);
 
   const startSpinning = () => {
+    if (isSpinning) {return;}
+    // Crear un nuevo elemento de audio
+    const audio_giro = new Audio(roulette_song);
+    const audio_shot = new Audio(reload_shot_song);
+
     setSpinning(true);
 
     // Genera un número aleatorio entre 30 y 50 para determinar el número de giros
@@ -21,26 +28,33 @@ const Russian_Roulette = () => {
     let totalRotation = spins * 60;
 
     // Aplica la rotación en CSS usando la propiedad transform
-    document.querySelector('.roulette').style.transition = 'transform 3s ease-out';
+    document.querySelector('.roulette').style.transition = 'transform 3.1s ease-out';
     document.querySelector('.roulette').style.transform = `rotate(${totalRotation}deg)`;
+
+    // Reproduce el sonido al iniciar el giro
+    audio_giro.play();
 
     // Simula la detención después de 3 segundos
     setTimeout(() => {
-      //volvemos a poner la imagen en la posición pero sin que se vea que gire
-      let girofinal = (totalRotation/360);
+      // Volvemos a poner la imagen en la posición pero sin que se vea que gire
+      let girofinal = (totalRotation / 360);
       girofinal = Math.trunc(girofinal);
       girofinal = girofinal * 360;
-      girofinal = totalRotation-girofinal;
+      girofinal = totalRotation - girofinal;
       console.log(girofinal);
+
       // Restablecer la rotación a la posición inicial
       document.querySelector('.roulette').style.transition = 'none'; // Desactivar transición
       document.querySelector('.roulette').style.transform = `rotate(${girofinal}deg)`; // Restablecer a la posición inicial
       setSpinning(false);
-      if(girofinal === 0){
+
+      if (girofinal === 0) {
+        audio_shot.play();
         confetti();
       }
     }, 3000);
-};
+  };
+
   return (
     <>
       <div>
@@ -53,12 +67,10 @@ const Russian_Roulette = () => {
         <div className={`roulette ${isSpinning ? 'spinning' : ''}`}>
           <img src={roulette} alt="Ruleta" onClick={startSpinning}/>
         </div>
-        <button onClick={startSpinning} disabled={isSpinning} className="button_normal" style={{ position: "relative", zIndex: "1", marginTop: "100px", width:"300px", height:"75px", fontSize:"1.2rem"}}>
-          {isSpinning ? 'Spining...' : '🔫 Spin Russian Roulette 🔫'}
+        <button onClick={startSpinning} disabled={isSpinning} className="button_normal" style={{ position: "relative", zIndex: "1", marginTop: "100px", width: "300px", height: "75px", fontSize: "1.2rem" }}>
+          {isSpinning ? 'Spinning...' : '🔫 Spin Russian Roulette 🔫'}
         </button>
-        {/* <div>{isSpinning ? randomSpins : null}</div> */}
         {isSpinning && <div>Good Luck!</div>}
-        {/* <div>{isSpinning.toString()}</div> */}
       </div>
     </>
   );
