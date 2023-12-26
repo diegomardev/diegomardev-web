a1faf9952e3886751c93566cf3c694038e36ef6f
diegomarprogramacion <diegomarprogramacion@gmail.com>
2023-12-26 18:03:34 +0100

    ok candles

--- src/pages/Apps/Birthday_Cake/Birthday_Cake.jsx
+++ src/pages/Apps/Birthday_Cake/Birthday_Cake.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import Navbar from '../../../components/Navbar/Navbar';
 import confetti from 'canvas-confetti';
 import { candles_init } from './candles';
@@ -6,6 +6,11 @@ import './Birthday_Cake.css';
 
 function Birthday_Cake() {
   const [candles, setCandles] = useState(candles_init);
+  const [blowButtonPressed, setBlowButtonPressed] = useState(false);
+  const [litCandlesCount, setLitCandlesCount] = useState(0); // Nuevo estado para contar las velas encendidas
+
+  const blowButtonPressedRef = useRef(blowButtonPressed);
+
   const addCandle = (event) => {
     const boundingRect = event.currentTarget.getBoundingClientRect();
     const newCandle = {
@@ -26,10 +31,20 @@ function Birthday_Cake() {
       for (let i = 0; i < diff; i++) {
         addCandleRandom();
       }
-    } else if (newCandlesCount < currentCandlesCount) {
+    } else if (newCandlesCount === currentCandlesCount-1) {
       // Quitar velas
       const diff = currentCandlesCount - newCandlesCount;
       setCandles(candles.slice(0, currentCandlesCount - diff));
+      // Actualizar el conteo de velas encendidas
+      if(litCandlesCount > 0)
+      setLitCandlesCount((prevCount) => prevCount - 1);
+    }
+    else if(newCandlesCount < currentCandlesCount){
+      // Quitar velas
+      const diff = currentCandlesCount - newCandlesCount;
+      setCandles(candles.slice(0, currentCandlesCount - diff));
+      // Actualizar el conteo de velas encendidas
+      setLitCandlesCount(currentCandlesCount - diff);
     }
   };
   const addCandleRandom = () => {
@@ -37,24 +52,54 @@ function Birthday_Cake() {
       id: candles.length + 1,
       top: (Math.random() * 30)-15, // Posición vertical aleatoria
       left: (Math.random() * 90)+5, // Posición horizontal aleatoria
+      isBlown: false,
     };
     setCandles([...candles, newCandle]);
   };
+  const blowCandles = () => {
+    // Iniciar la secuencia de apagado de velas
+    setBlowButtonPressed(true);
+  };
+  function encender_velas() {
+    const nuevasVelas = candles.map((candle) => ({
+      ...candle,
+      isBlown: false,
+    }));
+    setCandles(nuevasVelas);
+    setLitCandlesCount(0);
+  }
+  const blowCandlesSequentially = (index) => {
+    setTimeout(() => {
+      if (index < candles.length && blowButtonPressedRef.current) {
+        setCandles((prevCandles) => {
+          const updatedCandles = [...prevCandles];
+          updatedCandles[index] = { ...updatedCandles[index], isBlown: true };
+          return updatedCandles;
+        });
 
+        if (!blowButtonPressedRef.current) {
+          console.log("Blow button released");
+          clearTimeout(timeout);
+        }
+
+        // Actualizar el conteo de velas encendidas
+        setLitCandlesCount((prevCount) => prevCount + 1);
+
+        blowCandlesSequentially(index + 1);
+      }
+    }, 100);
+  };
+
+useEffect(() => {
+  blowButtonPressedRef.current = blowButtonPressed;
+}, [blowButtonPressed]);
   return (
     <>
       <div>
         <Navbar />
       </div>
-      Years old
-      <input
-        type="number"
-        value={candles.length}
-        onChange={handleInputChange}
-        min="0"
-        max="150"
-      />
-      <div className="cake" >
+      <h1 className="read-the-docs">🎂 Happy Birthday! 🎂</h1>
+      <div className="cake">
         <div className="plate"></div>
         <div className="layer layer-bottom"></div>
         <div className="layer layer-middle"></div>
@@ -67,11 +112,48 @@ function Birthday_Cake() {
           <div
             key={candle.id}
             className={`candle ${candle.id === candles.length ? "fall" : ""}`}
-            style={{ top: `${candle.top}%`, left: `${candle.left}%`, zIndex:`${Math.round(candle.top)+20}`}}
+            style={{
+              top: `${candle.top}%`,
+              left: `${candle.left}%`,
+              zIndex: `${Math.round(candle.top) + 20}`,
+            }}
           >
-            <div className="flame"></div>
+            {!candle.isBlown && <div className="flame"></div>}
           </div>
         ))}
+      </div>
+      <div className='posicion_texto'>
+        <div>
+          Years old
+          <input
+            type="number"
+            value={candles.length}
+            onChange={handleInputChange}
+            min="0"
+            max="150"
+          />
+        </div>
+
+        <div>
+          Blown out candles: {litCandlesCount} -
+          <button
+            onMouseDown={() => {
+              setBlowButtonPressed(true);
+              blowCandlesSequentially(litCandlesCount);
+            }}
+            onMouseUp={() => setBlowButtonPressed(false)}
+            onTouchStart={() => {
+              setBlowButtonPressed(true);
+              blowCandlesSequentially(litCandlesCount);
+            }}
+            onTouchEnd={() => setBlowButtonPressed(false)}
+          >
+            Blow candles 💨 🎂
+          </button>
+        </div>
+        <div>
+          <button onClick={encender_velas}>Encender velas 🕯️</button>
+        </div>
       </div>
     </>
   );
