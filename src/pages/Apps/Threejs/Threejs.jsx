497c20c2f5709a1a27799a710e5bbeeaa158c880
diegomarprogramacion <diegomarprogramacion@gmail.com>
2024-01-17 09:13:37 +0100

    import stl drag and drop and with button

--- src/pages/Apps/Threejs/Threejs.jsx
+++ src/pages/Apps/Threejs/Threejs.jsx
@@ -2,11 +2,10 @@ import React, { useEffect, useRef } from 'react';
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { STLLoader } from 'three/examples/jsm/loaders/STLLoader';
-import Stats from 'three/examples/jsm/libs/stats.module';
-import Navbar from '../../../components/Navbar/Navbar';
 
 const ThreeJSModel = () => {
   const canvasRef = useRef();
+  const fileInputRef = useRef();
 
   useEffect(() => {
     const scene = new THREE.Scene();
@@ -22,59 +21,102 @@ const ThreeJSModel = () => {
 
     const renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
-    renderer.setClearColor(0x242424, 1); //color de fondo de la escena
+    renderer.setClearColor(0x242424, 1);
     canvasRef.current.appendChild(renderer.domElement);
 
     const controls = new OrbitControls(camera, renderer.domElement);
     controls.enableDamping = true;
 
-    const material1 = new THREE.MeshBasicMaterial({ color: 0xffffff, wireframe: true });
     const material2 = new THREE.MeshNormalMaterial();
     const loader = new STLLoader();
-    loader.load(
-      'models/cubo.stl',
-      function (geometry) {
-        geometry.center();
-        const mesh1 = new THREE.Mesh(geometry, material1);
-        const mesh2 = new THREE.Mesh(geometry, material2);
-        //scene.add(mesh1);//ocultamos el objeto de lineas
-        scene.add(mesh2);
-      },
-      (xhr) => {
-        console.log((xhr.loaded / xhr.total) * 100 + '% loaded');
-      },
-      (error) => {
-        console.log(error);
+    // Función para limpiar la escena
+    function clearScene() {
+      scene.children.forEach((child) => {
+        if (child instanceof THREE.Mesh) {
+          scene.remove(child);
+        }
+      });
+    }
+    // Función para cargar STL
+    const loadSTL = (geometry) => {
+      geometry.center();
+      const mesh = new THREE.Mesh(geometry, material2);
+      scene.add(mesh);
+    };
+
+    // Cargar el archivo "cubo.stl" la primera vez
+    loader.load('models/cubo.stl', loadSTL, undefined, (error) => {
+      console.error('Error loading cubo.stl:', error);
+    });
+
+    // Función para manejar cambios en la entrada de archivo
+    const handleFileInputChange = (event) => {
+      const file = event.target.files[0];
+      if (file) {
+        clearScene();
+        loadSTLFile(file);
       }
-    );
+    };
 
+    // Función para manejar el evento de arrastrar y soltar
+    const handleFileDrop = (event) => {
+      event.preventDefault();
+      const file = event.dataTransfer.files[0];
+      if (file) {
+        clearScene();
+        loadSTLFile(file);
+      }
+    };
 
-    //const stats = new Stats();
-    //document.body.appendChild(stats.dom);
+    // Función para cargar un archivo STL
+    const loadSTLFile = (file) => {
+      const reader = new FileReader();
+      reader.onload = (event) => {
+        const geometry = loader.parse(event.target.result);
+        loadSTL(geometry);
+      };
+      reader.readAsArrayBuffer(file);
+    };
 
+    // Agregar eventos para manejar el arrastrar y soltar
+    canvasRef.current.addEventListener('dragover', (event) => event.preventDefault());
+    canvasRef.current.addEventListener('drop', handleFileDrop);
+
+    // Agregar eventos para manejar el cambio de entrada de archivo
+    fileInputRef.current.addEventListener('change', handleFileInputChange);
+
+    // Función de animación
     function animate() {
-      // Rotar ambos cubos
       scene.rotation.y += 0.001;
       requestAnimationFrame(animate);
       controls.update();
-      render();
-      //stats.update();
-    }
-    function render() {
       renderer.render(scene, camera);
     }
-    animate();
-  }, []); // Empty dependency array ensures useEffect runs only once on mount
 
-  return(
+    animate();
+
+    return () => {
+      // Limpieza de eventos
+      canvasRef.current.removeEventListener('dragover', (event) => event.preventDefault());
+      canvasRef.current.removeEventListener('drop', handleFileDrop);
+      fileInputRef.current.removeEventListener('change', handleFileInputChange);
+    };
+  }, []);
+
+  return (
     <>
-      <div>
-        <Navbar />
-      </div>
-      <h1 className="read-the-docs">STL Viewer</h1>
+      <h1>STL Viewer</h1>
+      <input
+        type="file"
+        id="stl_uploads"
+        name="stl_uploads"
+        accept=".stl"
+        className="input_image"
+        ref={fileInputRef}
+      />
       <div ref={canvasRef} />
     </>
-  )
+  );
 };
 
 export default ThreeJSModel;
