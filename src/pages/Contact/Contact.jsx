d03fa143c10c191a6f778bdd5b3dad66fa945b37
Diego Martínez <120572223+diegomardev@users.noreply.github.com>
2026-02-11 18:20:42 +0100

    Update contact links with target and rel attributes

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -14,21 +14,43 @@ function Contact() {
         Contact
       </h1>
       <div className='contact'>
-        <a href="https://www.linkedin.com/in/diegomarbar/" className="contact_elements">
+        <a
+          href="https://www.linkedin.com/in/diegomarbar/"
+          className="contact_elements"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <IconBrandLinkedin size={100} />
         </a>
-        <a href="https://github.com/diegomardev" className="contact_elements">
+
+        <a
+          href="https://github.com/diegomardev"
+          className="contact_elements"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <IconBrandGithub size={100} />
         </a>
-        <a href="mailto:diegomarbar@gmail.com" className="contact_elements">
+
+        <a
+          href="mailto:diegomarbar@gmail.com"
+          className="contact_elements"
+        >
           <IconMail size={100} />
         </a>
-        <a href="https://www.instagram.com/_.diegomar._" className="contact_elements">
+
+        <a
+          href="https://www.instagram.com/diegomarfpv"
+          className="contact_elements"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <IconBrandInstagram size={100} />
         </a>
       </div>
     </>
   );
 }
+
 
 export default Contact;
